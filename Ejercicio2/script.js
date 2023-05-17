bf1c4ef0c3618ab10196f612688c484743234489
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-05-16 22:17:26 -0500

    Se creo script para el ejercicio 2

---
+++ Ejercicio2/script.js
@@ -0,0 +1,13 @@
+
+
+    //La funcion split nos almacena en un array el string en caracteres
+    var caracteres=texto.split('');
+    //el reverse cambia de orden al array
+    var elementosInvertidos=caracteres.reverse();
+    //el join devuelve una matriz unida
+    var palabraInvertida=elementosInvertidos.join('');
+ 
+    
+
+
+
