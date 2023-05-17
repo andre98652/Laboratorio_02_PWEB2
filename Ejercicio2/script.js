430d9632e0bf6efd9dff35adebbd5167e84dbf0b
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-05-16 22:35:15 -0500

    Se mejoro el codigo del script del ejercicio 2

--- Ejercicio2/script.js
+++ Ejercicio2/script.js
@@ -1,13 +1,31 @@
+//primero capturamos los elemetos "ID"
+var boton=document.getElementById("boton")
+var parrafo=document.getElementById("invertido")
 
+boton.addEventListener('click',invertir);
 
+function invertir(){
+
+    let texto=document.getElementById("cajaTexto").value;
+    let invertido="";   
+    //usamos un for a la inversa 
+    for(let i=texto.length-1;i>=0;i--){
+        invertido= invertido + texto.charAt(i);
+    }
+    
+    document.getElementById("invertido").innerHTML=invertido;
+////////////////////////////////////////////////////
+//LO MISMO PERO USANDO SOMO METODOS
+    
+/*
     //La funcion split nos almacena en un array el string en caracteres
     var caracteres=texto.split('');
     //el reverse cambia de orden al array
     var elementosInvertidos=caracteres.reverse();
     //el join devuelve una matriz unida
     var palabraInvertida=elementosInvertidos.join('');
- 
+ */
     
 
-
+}
 
